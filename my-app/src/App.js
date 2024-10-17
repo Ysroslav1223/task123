d404f6ffcdcba87e0ea001421e7970321ad31234
Yaroslav Zvonok <aroslavzvonok876@gmail.com>
2024-10-17 20:52:32 +0200

    task123.2 исправление

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,26 +10,27 @@ function App() {
     React.createElement(
       "header",
       { className: "App-header" },
+      React.createElement("img", {
+        className: "App-logo",
+        src: logo,
+        alt: "logo",
+      }),
       React.createElement(
-        "img",
-        { className: "App-logo", src: logo, alt: "logo" },
-        React.createElement(
-          "p",
-          "Edit",
-          React.createElement("code", null, "src/App.js"),
-          "and save to reload",
-          React.createElement(
-            "a",
-            {
-              className: "App-link",
-              href: "https://reactjs.org",
-              target: "_blank",
-              rel: "noopener noreferrer",
-            },
-            "Learn React",
-            React.createElement("p", { className: `Текущий год: ${year}` })
-          )
-        )
+        "p",
+        "Edit",
+        React.createElement("code", null, "src/App.js"),
+        "and save to reload."
+      ),
+      React.createElement(
+        "a",
+        {
+          className: "App-link",
+          href: "https://reactjs.org",
+          target: "_blank",
+          rel: "noopener noreferrer",
+        },
+        "Learn React",
+        React.createElement("p", null, `Текущий год: ${year}`)
       )
     )
   );
